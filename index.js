a6afe064c02a061ab6b27bb025a03ad0d4c226dc
Erza <exposedmail@protonmail.ch>
2021-01-14 18:33:07 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,39 @@
+require("dotenv").config();
+const { JSDOM } = require("jsdom");
+const fetch = require("node-fetch");
+const { Webhook, MessageBuilder } = require("discord-webhook-node");
+const webhook = new Webhook(process.env.WEBHOOK_URL);
+
+async function getLatestVersion() {
+  const response = await fetch(process.env.PAGE_URL);
+  const text = await response.text();
+  const dom = await new JSDOM(text);
+  const element = dom.window.document.querySelector(".div-table-body-BIOS > .download-version");
+
+  return element.textContent.replace(/\s/g, "");
+}
+
+let shouldCheck = true;
+setInterval(async () => {
+  if (!shouldCheck) {
+    return;
+  }
+
+  const latestVersion = await getLatestVersion();
+  if (latestVersion == process.env.INSTALLED_VERSION) {
+    return;
+  }
+
+  const embed = new MessageBuilder()
+  .setTitle("BIOS Update available!")
+  .setURL(process.env.PAGE_URL)
+  .setThumbnail(process.env.WEBHOOK_THUMBNAIL)
+  .addField("Installed version", process.env.INSTALLED_VERSION, true)
+  .addField("New version", latestVersion, true)
+  .setColor('#00b0f4')
+  .setTimestamp();
+
+  await webhook.send(`<@${process.env.OWNER_ID}>`);
+  await webhook.send(embed);
+  shouldCheck = false;
+}, process.env.CHECK_INTERVAL);
